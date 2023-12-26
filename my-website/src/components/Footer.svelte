a4fa294e142c7d5ac9bd8d48b892dd8940a9fef0
Brandon Tran <tranbrandon1233@gmail.com>
2023-12-26 12:47:05 -0800

    Finished website

---
+++ my-website/src/components/Footer.svelte
@@ -0,0 +1,53 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+    id="contact"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <!-- <div
+            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
+        >
+            <a>
+                <i class="fa-brands fa-linkedin-in" />
+            </a>
+            <a>
+                <i class="fa-brands fa-github" />
+            </a>
+        </div>
+        <p class="mx-auto text-sm opacity-70">or</p>
+        <p class="">tranbrandon1233@gmail.com</p> -->
+        <p><b class="pr-2">Email</b> tranbrandon1233@gmail.com</p>
+        <p>
+            <b class="pr-2">GitHub</b>
+            <a
+                href="https://github.com/tranbrandon1233"
+                target="_blank"
+                class="text-violet-400"
+                >tranbrandon1233<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a
+                href="https://www.linkedin.com/in/brandon-tran-bb264b187"
+                target="_blank"
+                class="text-violet-400"
+                >Brandon Tran<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+    </div>
+</footer>
